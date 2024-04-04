d9fd57e0bd9bbd5df103e15c51b401cbde58b227
John Carl <johnmurillo758@gmail.com>
2024-04-04 22:52:02 +0800

    Push update fixing not use import

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -1,27 +1,21 @@
 'use client';
 
 import * as React from 'react';
-import RouterLink from 'next/link';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Alert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
-import InputLabel from '@mui/material/InputLabel';
-import Link from '@mui/material/Link';
-import OutlinedInput from '@mui/material/OutlinedInput';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
-import { Eye as EyeIcon } from '@phosphor-icons/react/dist/ssr/Eye';
-import { EyeSlash as EyeSlashIcon } from '@phosphor-icons/react/dist/ssr/EyeSlash';
 import { Controller, useForm } from 'react-hook-form';
 import { z as zod } from 'zod';
 import Box from '@mui/material/Box';
-import { paths } from '@/paths';
 import { authClient } from '@/lib/auth/client';
 import { useUser } from '@/hooks/use-user';
 import '@/components/auth/style.css';
+
 const schema = zod.object({
   email: zod.string().min(1, { message: 'Email is required' }).email(),
   password: zod.string().min(1, { message: 'Password is required' }),
@@ -45,20 +39,14 @@ export function SignInForm(): React.JSX.Element {
   const onSubmit = React.useCallback(
     async (values: Values): Promise<void> => {
       setIsPending(true);
-
       const { error } = await authClient.signInWithPassword(values);
-
       if (error) {
         setError('root', { type: 'server', message: error });
         setIsPending(false);
         return;
       }
 
-      // Refresh the auth state
       await checkSession?.();
-
-      // UserProvider, for this case, will not refresh the router
-      // After refresh, GuestGuard will handle the redirect
       router.refresh();
     },
     [checkSession, router, setError]
@@ -80,7 +68,7 @@ export function SignInForm(): React.JSX.Element {
                   <FormControl error={Boolean(errors.email)}>
                     <Box className="field">
                       <input type="email" {...field} />
-                      <label>Email Address</label>
+                      <label htmlFor="email">Email Address</label>
                     </Box>
                     {errors.email ? <FormHelperText>{errors.email.message}</FormHelperText> : null}
                   </FormControl>
@@ -97,7 +85,7 @@ export function SignInForm(): React.JSX.Element {
                     <Stack>
                       <Stack className="field">
                         <input type="password" {...field} />
-                        <label>Password</label>
+                        <label htmlFor="password">Password</label>
                       </Stack>
                     </Stack>
                     {errors.password ? <FormHelperText>{errors.password.message}</FormHelperText> : null}
@@ -117,5 +105,3 @@ export function SignInForm(): React.JSX.Element {
     </Stack>
   );
 }
-
-
